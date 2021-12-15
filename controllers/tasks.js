6721cae6349b9d70df9ef7affd13d4c8e989faa6
arjun-1510 <Arjung1510@gmail.com>
2021-12-15 21:58:52 +0530

    delete crud

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -20,9 +20,7 @@ try {
     
 }
 
-const updateTask = (req,res) => {
-    res.send('update task')
-}
+
 
 const getTask = async (req,res) => {
     try {
@@ -38,8 +36,23 @@ const getTask = async (req,res) => {
     }
 }
 
-const deleteTask = (req,res) => {
-    res.send('DeleteTask')
+const updateTask = (req,res) => {
+    res.send('update task')
+}
+
+const deleteTask = async (req,res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if (!task) {
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({ msg: error})
+        
+    }
+    
 }
 
 module.exports = {getAllTasks,createTask,updateTask,getTask,deleteTask}
